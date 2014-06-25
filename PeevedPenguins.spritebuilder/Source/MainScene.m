9c6c25c9282c1fc18c7bc4d247b41ee0748b9c68
Dat Do <DatDo@Dats-MacBook-Pro.local>
2014-06-25 11:15:39 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/MainScene.m
+++ PeevedPenguins.spritebuilder/Source/MainScene.m
@@ -11,6 +11,7 @@
 @implementation MainScene
 - (void)play
 {
-    CCLOG(@"play button pressed");
+    CCScene *gameplayScene = [CCBReader loadAsScene:@"GamePlay"];
+    [[CCDirector sharedDirector] replaceScene:gameplayScene];
 }
 @end
