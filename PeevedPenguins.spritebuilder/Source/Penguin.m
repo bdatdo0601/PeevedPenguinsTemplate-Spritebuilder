328ff09848faedcbeed4458c0de9c94493bb7135
Dat Do <DatDo@Dats-MacBook-Pro.local>
2014-06-25 10:42:40 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -0,0 +1,22 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Dat Do on 6/25/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+- (id)init
+{
+    self = [super init];
+    if (self)
+    {
+        CCLOG(@"Penguin Created");
+    }
+    return self;
+}
+
+@end
