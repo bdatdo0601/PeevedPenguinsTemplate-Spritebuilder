f2cf9d17370f81b5a113265ff8ce1630de57620a
Dat Do <DatDo@Dats-MacBook-Pro.local>
2014-06-25 20:46:35 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/GamePlay.m
+++ PeevedPenguins.spritebuilder/Source/GamePlay.m
@@ -44,6 +44,12 @@
             _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
     }
 }
+- (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
+{
+        //whenever touches move, update the position of the mouseJointNode --> touch location
+    CGPoint touchLocation =[touch locationInNode:_contentNode];
+    _mouseJointNode.position = touchLocation;
+}
 - (void)retry
 {[[CCDirector sharedDirector] replaceScene: [CCBReader loadAsScene:@"GamePlay"]];
 }
