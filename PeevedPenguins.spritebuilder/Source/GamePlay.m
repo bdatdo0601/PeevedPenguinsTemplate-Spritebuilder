4f531f548b17cdabe29392426420d4d87857234d
Dat Do <DatDo@Dats-MacBook-Pro.local>
2014-06-26 09:20:13 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/GamePlay.m
+++ PeevedPenguins.spritebuilder/Source/GamePlay.m
@@ -27,6 +27,8 @@
     self.userInteractionEnabled = TRUE;
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
+    //visualize physics bodies & joints
+    _physicsNode.debugDraw = TRUE;
     //nothing shall collide
     _pullbackNode.physicsBody.collisionMask = @[];
     _mouseJointNode.physicsBody.collisionMask = @[];
